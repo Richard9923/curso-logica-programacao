4940376b007c1651d7123396c0ef79496cbd6696
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-05-21 18:59:02 -0300

    up

---
+++ script.js
@@ -0,0 +1,32 @@
+/*
+let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+let area;
+if (forma === 'retangulo') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+
+console.log(area);
+*/
+
+const forma = 'triangulo';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2
+}
+
+console.log(area);
+
+
+
+
+
