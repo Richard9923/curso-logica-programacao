e8d2bf40a31a5d7e494fcb4ccd11c65823f0d03f
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-05-21 20:16:41 -0300

    up

---
+++ boolean.js
@@ -0,0 +1,27 @@
+// boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy or falsy
+
+// 0 > false
+// 1 > true
+
+console.log(0 == false);
+console.log('' == false);
+console.log(1 == true);
+
+// null 
+// undefined
+
+let minhaVar; // == undefined caso vazia;
+let varNull = null;
+
+let numero = 3;
+let texto = 'Alura';
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
+
+
