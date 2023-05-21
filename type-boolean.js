4359b536935152f2d64d6a5f94865583ddb808e6
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-05-21 18:35:16 -0300

    Initial commit

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+// boolean 
+// true > verdadeiro
+// false > falso
+
+const primeiroNum = 5;
+const segundoNum = 10;
+const senha = 'batat123';
+
+// console.log(primeiroNum === segundoNum);
+
+const texto1 = 'Alura';
+const texto2 = 'alura';
+
+console.log(texto1 == texto2);
