90788047e0972404c3ee1517fee84973211161d9
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-05-22 18:39:10 -0300

    up

---
+++ arrow-function.js
@@ -0,0 +1,22 @@
+function apresentar (nome) {
+    return `meu nome é ${nome}`;
+}
+
+const apresentarArrow = (nome) => `meu nome é ${nome}`;
+const soma = (num1, num2) => num1 + num2;
+
+// Arrow function com mais de uma linha de instrução
+
+const somaNumerosPequenos = (num1, num2) => {
+
+    if (num1 || num2 > 10) {
+        return 'Somente números de 1 a 9'
+    } else {
+        return num1 + num2;
+    }
+};
+
+// Hoisting> arrow function se comporta como expressão
+
+// operador maior ou igual que 
+// >=
---
+++ exprecoes.js
@@ -0,0 +1,19 @@
+function minhaFuncao(param) {
+    // blodo de código
+}
+
+minhaFuncao('param');
+
+// exorecai de funcao
+
+// const soma = function (num1, num2) {return num1 + num2};
+console.log(soma(1,1));
+
+// diferenca principal
+
+console.log(apresentar());
+function apresentar () {
+    return 'olá';
+}
+console.log(soma(1,1));
+const soma = function (num1, num2) {return num1 + num2};
