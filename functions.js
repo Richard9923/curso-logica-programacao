825c8507e07c269daa5f6921ac906de6d434fb12
Raffael2323 <118739446+Raffael2323@users.noreply.github.com>
2023-05-22 18:12:58 -0300

    update

---
+++ functions.js
@@ -0,0 +1,23 @@
+
+
+
+// DECLARAÇÃO DE FUNÇÃO
+
+function imprimeTexto (texto) {
+    console.log(texto);
+}
+
+// EXECUTA A FUNÇÃO
+
+imprimeTexto(soma());
+imprimeTexto('Bom dia ');
+
+// três formas de escrever funcoes
+
+function soma (num1, num2) {
+    const soma = num1 + num2;
+    return console.log(soma);
+}
+
+soma(5,10)
+
--- termplate.js
+++ termplate.js
@@ -1,1 +1,7 @@
+const idadeMinima = 18;
+const idadeCliente = 22;
+
+const comparacao = idadeCliente >= idadeMinima ? 'Cerveja' : 'Suco';
+const texto = `O cliente com a idade de ${idadeCliente} anos, poderá tomar ${comparacao}.`;
+console.log(texto);
 
